1572b96943d1f2f804c11f8e04d50b93dd0fd8cb
Charlene Nicol <charln.nicol@gmail.com>
2022-11-16 16:45:04 +0100

    add all controllers and views for restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Seeding...'
+Restaurant.create(name: 'Fufu', address: '52 Cours Portal, Bordeaux', phone_number: '0557834149', category: 'japanese')
+Restaurant.create(name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '0557831213', category: 'italian')
+Restaurant.create(name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '0557834212', category: 'belgian')
+Restaurant.create(name: 'Martial', address: '11 Cours du vent, Terreau', phone_number: '0557565758', category: 'french')
+Restaurant.create(name: 'Zhang', address: '8 rue du faux, Arthes', phone_number: '0557834149', category: 'chinese')
+puts Restaurant.count
+puts 'Seeding done'
